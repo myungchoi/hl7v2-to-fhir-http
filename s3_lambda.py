bb1656046053ec9a5fb1b9014c6207c43f5f1e91
Saul Crumpton <saul.crumpton@gmail.com>
2021-04-23 16:21:53 -0400

    updating for working lambda code

--- s3_lambda.py
+++ s3_lambda.py
@@ -4,15 +4,16 @@ import json
 import uuid
 import logging
 import datetime
-import requests
-import smart_open
 import boto3
 import botocore
 import base64
+import urllib
 
 # HL7V2 message queue
 _Q_NAME = 'csels-xlr-hl7v2-translator-queue'
 _Q_URL  = 'https://sqs.us-east-1.amazonaws.com/681252169616/csels-xlr-hl7v2-translator-queue'  
+s3 = boto3.client('s3')
+_sqs = boto3.client('sqs')
 
 def s3_event(s3_event):
     """
@@ -21,39 +22,39 @@ def s3_event(s3_event):
     It opens the file, extracts the messages, and puts them on the
     NETSS message queue.
     """
-    app.log.debug('Received S3:ObjectCreated event for bucket {0}, key {1}'.
-                  format(s3_event.bucket, s3_event.key))
+    s3_bucket = event['Records'][0]['s3']['bucket']['name']
+    s3_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'],encoding='utf-8')
+    print('Received S3:ObjectCreated event for bucket {0}, key {1}'.
+                  format(s3_bucket, s3_key))
 
     # stores line numbers of invalid messages
     invalid_messages = []
     
-    filename = 's3://{0}/{1}'.format(s3_event.bucket, s3_event.key)
-    obj = s3.get_object(Bucket = s3_event.bucket, Key = s3_event.key) 
+    filename = 's3://{0}/{1}'.format(s3_bucket, s3_key)
+    obj = s3.get_object(Bucket = s3_ebucket, Key = s3_key) 
     hl7v2_message = obj['Body'].read().decode("utf-8")
     b64Message=base64.b64encode(hl7v2_message.encode())
     try:
         q_response = _sqs.send_message(
             QueueUrl = _Q_URL,
-            MessageBody = b64Message
+            MessageBody = b64Message.decode()
         )
-    except _sqs.meta.client.exceptions.InvalidMessageContents as err:
-        app.log.error(err.response)
-    except _sqs.meta.client.exceptions.UnsupportedOperation as err:
-        app.log.error(err.response)
+    except Exception as err:
+        print(err)
         
     if q_response is not None and 'MessageId' in q_response:
-        app.log.debug('Enqueued message: {0}, MessageId: {1}'.
-                      format(netss_msg, q_response['MessageId']))
+        print('Enqueued message: {0}, MessageId: {1}'.
+                      format(hl7v2_message, q_response['MessageId']))
             
     # Delete the file since all messages have been enqueued.
     # For some reason this doesn't work with the s3 client, so
     # using bucket object instead.
-    bucket = boto3.resource('s3').Bucket(_S3_UPLOAD_BUCKET)
+    bucket = boto3.resource('s3').Bucket(s3_bucket)
     s3_response = bucket.delete_objects(
         Delete = {
             'Objects': [
                 {
-                    'Key' : s3_event.key
+                    'Key' : s3_key
                 }
             ]
         }
@@ -64,9 +65,9 @@ def s3_event(s3_event):
         if 'Deleted' in s3_response:
             deleted_obj = s3_response['Deleted'][0]
             deleted_key = deleted_obj['Key']
-            if deleted_key == s3_event.key:
-                app.log.debug('Successfully deleted file {0}'.
-                              format(s3_event.key))
+            if deleted_key == s3_key:
+                print('Successfully deleted file {0}'.
+                              format(s3_key))
     else:
-        app.log.error('S3 File delete failed: ')
-        app.log.error(s3_response)
+        print('S3 File delete failed: ')
+        print(s3_response)
